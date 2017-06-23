9f7691406ba2fd3dc82ac936924e846eb534f1eb
Vadim Ivakh <ivakhvadim@gmail.com>
2017-06-23 14:09:46 +0300

    added "add_like" view without protection

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^posts/all', views.all_posts),
-    url(r'^post/get/(?P<post_id>[0-9]+)/$', views.post_by_id),
+    url(r'^$', views.all_posts),
+    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_by_id),
+    url(r'^posts/addlike/(?P<post_id>[0-9]+)/$', views.add_like),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http.response import HttpResponse
-from django.shortcuts import render_to_response
+from django.http.response import HttpResponse, Http404
+from django.shortcuts import render_to_response, redirect
 from blog.models import Post, Comment
-
-
-def index(request):
-    return render_to_response('main.html')
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def all_posts(request):
@@ -15,4 +12,13 @@ def all_posts(request):
 
 def post_by_id(request, post_id=1):
     return render_to_response('post_by_id.html', {'post': Post.objects.get(id=post_id), 'comments': Comment.objects.filter(comment_post_id=post_id)})
-    # return render_to_response('post_by_id.html', {'post': Post.objects.get(id=post_id)})
+
+
+def add_like(request, post_id):
+    try:
+        post = Post.objects.get(id = post_id)
+        post.post_likes += 1
+        post.save()
+    except ObjectDoesNotExist:
+        raise Http404
+    return redirect('/')
